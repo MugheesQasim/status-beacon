b814e32674c89a29caa5a0b9ba82a6ab824efb1c
Mughees Qasim <mughees.qasim.mq@gmail.com>
2024-12-27 15:30:32 +0200

    Add jest framework for frontend testing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Website Status heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Website Status/i);
+  expect(headingElement).toBeInTheDocument();
 });
